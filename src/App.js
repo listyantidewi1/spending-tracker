6fa77ddd44017c7478d3071d674a8876df61ea92
listyantidewi1 <listyantidewi@gmail.com>
2023-08-03 08:31:02 +0700

    add first styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>It's also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
